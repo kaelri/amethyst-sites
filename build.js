41084cfe7714f448ecda4332e7fc71ffa905c45f
Michael Engard <kaelri@gmail.com>
2021-10-29 09:52:00 -0400

    Refactoring & improvements.

--- build.js
+++ build.js
@@ -3,207 +3,200 @@ const YAML     = require('yaml')
 const pem      = require('pem')
 const { exec } = require('child_process')
 
-// GET SITES CONFIG
-const config = YAML.parse( fs.readFileSync('./config.yml', 'utf8') )
+class amethystSites {
 
-// HOSTS
-function buildHosts() {
-
-	let hosts = fs.readFileSync(config.hosts.path, 'utf8')
-
-	const lines = []
-	lines.push( `# begin ${config.name}` )
-	let urls = []
-
-	for (let i = 0; i < config.sites.length; i++) {
-		const site = config.sites[i]
-
-		urls.push( site.domain )
-
-		if ( site.alias ) urls.push( site.alias )
-
+	constructor() {
+		this.config = YAML.parse( fs.readFileSync('./config.yml', 'utf8') )
 	}
 
-	for (let i = 0; i < urls.length; i++) {
-		const url = urls[i]
-
-		lines.push( ''                   )
-		lines.push( `127.0.0.1 ${url}`   )
-		lines.push( `::1 ${url}`         )
-		lines.push( `fe80::1%lo0 ${url}` )
-		
+	build() {
+		this.buildHosts()
+		this.buildApacheVirtualHosts()
+		this.buildSSLCertificate()
 	}
 
-	lines.push( ''               )
-	lines.push( `# end ${config.name}` )
+	// HOSTS
+	buildHosts() {
 
-	const linesRegex = new RegExp( `/(# begin ${config.name}\n.*\n# end ${config.name})/s` )
-	let matches = hosts.match( linesRegex )
+		let hosts = fs.readFileSync( this.config.hosts.path, 'utf8' )
 
-	if ( matches ) {
-		hosts = hosts.replace( linesRegex, lines.join("\n") )
-	} else {
-		hosts += "\n\n" + lines.join("\n") + "\n\n"
-	}
+		const lines = [
+			`# begin ${this.config.name}`
+		]
 
-	fs.writeFileSync( config.hosts.path, hosts )
+		const urls = []
 
-}
+		for (let i = 0; i < this.config.sites.length; i++) {
+			const site = this.config.sites[i]
 
-// APACHE VIRTUAL HOSTS
-function buildApacheVirtualHosts() {
+			urls.push( site.domain )
 
-	let vhosts = fs.readFileSync( config.apache.vhostsPath, 'utf8' )
+			if ( site.alias ) urls.push( site.alias )
 
-	const lines = []
-	lines.push( `# begin ${config.name}` )
-
-	// LOCALHOST
-	lines.push( ''                                                  )
-	lines.push( '<VirtualHost *:443>'                               )
-	lines.push( `    DocumentRoot "${config.apache.documentRoot}"`  )
-	lines.push( '    SSLEngine on'                                  )
-	lines.push( `    SSLCertificateFile "${config.ssl.certPath}"`   )
-	lines.push( `    SSLCertificateKeyFile "${config.ssl.keyPath}"` )
-	lines.push( '</VirtualHost>'                                    )
-	lines.push( ''                                                  )
-	lines.push( '<VirtualHost *:80>'                                )
-	lines.push( `    DocumentRoot "${config.apache.documentRoot}"`  )
-	lines.push( '</VirtualHost>'                                    )
-
-	for (let i = 0; i < config.sites.length; i++) {
-		const site = config.sites[i]
-
-		if ( !site.server || site.server !== 'apache' ) continue;
-
-		lines.push( ''                                )
-		lines.push( `<VirtualHost *:443>`             )
-		lines.push( `    DocumentRoot "${site.path}"` )
-		lines.push( `    ServerName ${site.domain}`   )
-
-		if ( site.alias && site.alias.length ) {
-		lines.push( `    ServerAlias ${site.alias}` )
 		}
 
-		lines.push( `    SSLEngine on`                                  )
-		lines.push( `    SSLCertificateFile "${config.ssl.certPath}"`   )
-		lines.push( `    SSLCertificateKeyFile "${config.ssl.keyPath}"` )
+		for (let i = 0; i < urls.length; i++) {
+			const url = urls[i]
 
-		if ( site.errorLog && site.errorLog.length ) {
-		lines.push( `    ErrorLog "${site.errorLog}"` )
-		}
+			lines.push( ''                   )
+			lines.push( `127.0.0.1 ${url}`   )
+			lines.push( `::1 ${url}`         )
+			lines.push( `fe80::1%lo0 ${url}` )
 
-		if ( site.directory && site.directory.length ) {
-		lines.push( `    <Directory "${site.path}">` )
-		for (let i = 0; i < site.directory.length; i++) {
-		lines.push( `        ${site.directory[i]}` )
 		}
-		lines.push( `    </Directory>` )
-		}
-
-		lines.push( `</VirtualHost>` )
 
 		lines.push( '' )
+		lines.push( `# end ${this.config.name}` )
 
-		lines.push( `<VirtualHost *:80>`              )
-		lines.push( `    DocumentRoot "${site.path}"` )
-		lines.push( `    ServerName ${site.domain}`   )
+		const pattern = new RegExp( `(# begin ${this.config.name}\n.*\n# end ${this.config.name})`, 's' ),
+			  matches = hosts.match( pattern )
 
-		if ( site.alias && site.alias.length ) {
-		lines.push( `    ServerAlias ${site.alias}` )
+		if ( matches ) {
+			hosts = hosts.replace( pattern, lines.join("\n") )
+		} else {
+			hosts += "\n\n" + lines.join("\n") + "\n\n"
 		}
 
-		if ( site.errorLog && site.errorLog.length ) {
-		lines.push( `    ErrorLog "${site.errorLog}"` )
-		}
-
-		if ( site.directory && site.directory.length ) {
-		lines.push( `    <Directory "${site.path}">` )
-		for (let i = 0; i < site.directory.length; i++) {
-		lines.push( `        ${site.directory[i]}` )
-		}
-		lines.push( `    </Directory>` )
-		}
-
-		lines.push( `</VirtualHost>` )
+		fs.writeFileSync( this.config.hosts.path, hosts )
 
 	}
 
-	lines.push( '' )
-	lines.push( `# end ${config.name}` )
+	// APACHE VIRTUAL HOSTS
+	buildApacheVirtualHosts() {
 
-	const linesRegex = new RegExp( `/(# begin ${config.name}\n.*\n# end ${config.name})/s` )
-	let matches = vhosts.match( linesRegex )
+		let vhosts = fs.readFileSync( this.config.apache.vhostsPath, 'utf8' )
 
-	if ( matches ) {
-		vhosts = vhosts.replace( linesRegex, lines.join("\n") )
-	} else {
-		vhosts += "\n\n" + lines.join("\n") + "\n\n"
-	}
+		const lines = [
+			`# begin ${this.config.name}`
+		]
 
-	fs.writeFileSync( config.apache.vhostsPath, vhosts )
+		// LOCALHOST
+		lines.push( ''                                                       )
+		lines.push( '<VirtualHost *:443>'                                    )
+		lines.push( `    DocumentRoot "${this.config.apache.documentRoot}"`  )
+		lines.push( '    SSLEngine on'                                       )
+		lines.push( `    SSLCertificateFile "${this.config.ssl.certPath}"`   )
+		lines.push( `    SSLCertificateKeyFile "${this.config.ssl.keyPath}"` )
+		lines.push( '</VirtualHost>'                                         )
+		lines.push( ''                                                       )
+		lines.push( '<VirtualHost *:80>'                                     )
+		lines.push( '    Redirect 301 / https://localhost/'                  )
+		lines.push( '</VirtualHost>'                                         )
 
-}
+		for (let i = 0; i < this.config.sites.length; i++) {
+			const site = this.config.sites[i]
 
-function buildSSLCertificate() {
+			if ( !site.server || site.server !== 'apache' ) continue;
 
-	let altNames = [
-		'localhost'
-	]
+			// HTTPS
+			lines.push( ''                                )
+			lines.push( `<VirtualHost *:443>`             )
+			lines.push( `    DocumentRoot "${site.path}"` )
+			lines.push( `    ServerName ${site.domain}`   )
 
-	for (let i = 0; i < config.sites.length; i++) {
-		const site = config.sites[i]
-
-		altNames.push( site.domain )
-
-		if ( site.alias ) altNames.push( site.alias )
-
-	}
-
-	pem.createCSR({
-		commonName:   `${config.name} Self-Signed SSL Certificate`,
-		country:      config.ssl.country,
-		state:        config.ssl.state,
-		organization: config.ssl.organization,
-		emailAddress: config.ssl.email,
-		altNames:     altNames
-	}, ( err, result ) => {
-
-		pem.createCertificate({
-			selfSigned: true,
-			csr:        result.csr,
-			clientKey:  result.clientKey,
-		}, (err, keys) => {
-	
-			if ( err ) {
-				console.log(err);
-				return;
+			if ( site.alias && site.alias.length ) {
+			lines.push( `    ServerAlias ${site.alias}` )
 			}
-	
-			const key  = keys.serviceKey
-			const cert = keys.certificate
-	
-			fs.writeFileSync( config.ssl.keyPath, key  )
-			fs.writeFileSync( config.ssl.certPath, cert )
-	
-			let shellCommands = [
-				`security delete-certificate -c "${config.name} Self-Signed SSL Certificate" /Library/Keychains/System.keychain`,
-				`security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "${config.ssl.certPath}"`,
-				'brew services restart httpd'
-			]
-	
-			exec( shellCommands.join(';'), (error, stdout, stderr) => {
 
-				console.log(stdout)
-				
+			lines.push( `    SSLEngine on`                                  )
+			lines.push( `    SSLCertificateFile "${this.config.ssl.certPath}"`   )
+			lines.push( `    SSLCertificateKeyFile "${this.config.ssl.keyPath}"` )
+
+			if ( site.errorLog && site.errorLog.length ) {
+			lines.push( `    ErrorLog "${site.errorLog}"` )
+			}
+
+			if ( site.directory && site.directory.length ) {
+			lines.push( `    <Directory "${site.path}">` )
+			for (let i = 0; i < site.directory.length; i++) {
+			lines.push( `        ${site.directory[i]}` )
+			}
+			lines.push( `    </Directory>` )
+			}
+
+			lines.push( `</VirtualHost>` )
+
+			lines.push( '' )
+
+			// HTTP
+			lines.push( `<VirtualHost *:80>`                          )
+			lines.push( `    ServerName ${site.domain}`               )
+			lines.push( `    ServerAlias ${site.alias}`               )
+			lines.push( `    Redirect 301 / https://${site.domain}/ ` )
+			lines.push( `</VirtualHost>` )
+
+		}
+
+		lines.push( '' )
+		lines.push( `# end ${this.config.name}` )
+
+		const pattern = new RegExp( `(# begin ${this.config.name}\n.*\n# end ${this.config.name})`, 's' ),
+		      matches = vhosts.match( pattern )
+
+		if ( matches ) {
+			vhosts = vhosts.replace( pattern, lines.join("\n") )
+		} else {
+			vhosts += "\n\n" + lines.join("\n") + "\n\n"
+		}
+
+		fs.writeFileSync( this.config.apache.vhostsPath, vhosts )
+
+	}
+
+	buildSSLCertificate() {
+
+		let altNames = [
+			'localhost'
+		]
+
+		for (let i = 0; i < this.config.sites.length; i++) {
+			const site = this.config.sites[i]
+
+			altNames.push( site.domain )
+
+			if ( site.alias ) altNames.push( site.alias )
+
+		}
+
+		pem.createCSR({
+			commonName:   `${this.config.name} Self-Signed SSL Certificate`,
+			country:      this.config.ssl.country,
+			state:        this.config.ssl.state,
+			organization: this.config.ssl.organization,
+			emailAddress: this.config.ssl.email,
+			altNames:     altNames
+		}, ( err, result ) => {
+
+			pem.createCertificate({
+				selfSigned: true,
+				csr:        result.csr,
+				clientKey:  result.clientKey,
+			}, (err, keys) => {
+
+				if ( err ) {
+					console.log(err);
+					return;
+				}
+
+				const key  = keys.serviceKey
+				const cert = keys.certificate
+
+				fs.writeFileSync( this.config.ssl.keyPath,  key  )
+				fs.writeFileSync( this.config.ssl.certPath, cert )
+
+				let shellCommands = [
+					`security delete-certificate -c "${this.config.name} Self-Signed SSL Certificate" /Library/Keychains/System.keychain`,
+					`security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "${this.config.ssl.certPath}"`,
+				]
+
+				exec( shellCommands.join(';'), (error, stdout, stderr) => console.log(stdout) )
+
 			})
-		
+
 		})
-	
-	})
+
+	}
 
 }
 
-buildHosts()
-buildApacheVirtualHosts()
-buildSSLCertificate()
+( new amethystSites ).build()
